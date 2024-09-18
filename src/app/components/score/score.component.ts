33d38bd668579c2cd979a919e57c3cd513f641c0
Bryan Moreno <abre.go@outlook.com>
2024-09-18 17:01:53 -0600

    feat: score page

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { UserService } from '../../user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-score',
     templateUrl: './score.component.html',
     styleUrl: './score.component.scss'
 })
-export class ScoreComponent implements OnInit {
+export class ScoreComponent implements OnInit, AfterContentChecked {
 
     currentUser: any;
+    result: boolean = false;
+    value: string = '';
 
     constructor(
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private readonly route: ActivatedRoute,
     ) { }
 
     ngOnInit(): void {
         this.currentUser = this.userService.currentUser;
-    }  
+    }
+
+    ngAfterContentChecked(): void {
+        const { result, value } = this.route.snapshot.queryParams;
+        this.result = result === 'true';
+        this.value = value;
+    }
 }
